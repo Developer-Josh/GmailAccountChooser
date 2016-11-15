4a3be0f1e7901562e3445b2e7ada898a0e3ec92a
Developer-Josh <rei.r.joshua@gmail.com>
2016-11-15 15:56:53 -0500

    Created toggleAction function
    Optimized app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,65 +1,53 @@
 $(document).ready(function() {	
+	
+	var $container0 = $(".container-0");
+	var $container1 = $(".container-1");
+	var $container2 = $(".container-2");
+	var $email0 = $(".email-0");
+	var $email1 = $(".email-1");
+	var $email2 = $(".email-2");
+
 	//Begin Keyboard Shortcut Functions
-	//Shift + o Open all tab containers
+	//Shift + o expand all tab containers
 	Mousetrap.bind('shift+o', function() {
-		$(".email-0").fadeToggle();
-		$(".container-0").toggleClass("closed");
-		$(".container-0").toggleClass("open");
-		
-		$(".email-1").fadeToggle();
-		$(".container-1").toggleClass("closed");
-		$(".container-1").toggleClass("open");
-		
-		$(".email-2").fadeToggle();
-		$(".container-2").toggleClass("closed");
-		$(".container-2").toggleClass("open");
+		toggleAction($email0, $container0)
+		toggleAction($email1, $container1)
+		toggleAction($email2, $container2)
+
 	});
 	
-	//Shift + 0 Open Container 0
+	//Shift + 0 expand Container 0
 	Mousetrap.bind('shift+0', function() {
-		$(".email-0").fadeToggle();
-			$(".container-0").toggleClass("closed");
-			$(".container-0").toggleClass("open");
+		toggleAction($email0, $container0)
 	});
 	
-	//Shift + 1 Open Container 1
+	//Shift + 1 expand Container 1
 	Mousetrap.bind('shift+1', function() {
-		$(".email-1").fadeToggle();
-			$(".container-1").toggleClass("closed");
-			$(".container-1").toggleClass("open");
+		toggleAction($email1, $container1)
 	});
 	
-	//Shift + 1 Open Container 2
+	//Shift + 1 expand Container 2
 	Mousetrap.bind('shift+2', function() {
-		$(".email-2").fadeToggle();
-			$(".container-2").toggleClass("closed");
-			$(".container-2").toggleClass("open");
+		toggleAction($email2, $container2)
 	});
 	//End Keyboard Shorcut Functions
 	
-	//Click function for opening container-0
-	var container = $(".container-0");
-	container.click(function(){
-		$(".email-0").fadeToggle();
-		$(".container-0").toggleClass("closed");
-		$(".container-0").toggleClass("open");
+	//Click function for opening containers
+	function toggleAction ($email, $container) {
+		$email.fadeToggle();
+		$container.toggleClass("closed");
+		$container.toggleClass("open");
+	};
+
+	$container0.click(function() {
+		toggleAction($email0, $container0)
 	});
 	
-	//Click function for opening container-1
-	var container = $(".container-1");
-	container.click(function(){
-		$(".email-1").fadeToggle();
-		$(".container-1").toggleClass("closed");
-		$(".container-1").toggleClass("open");
+	$container1.click(function() {
+		toggleAction($email1, $container1)
 	});
 	
-	//Click function for opening container-2
-	var container = $(".container-2");
-	container.click(function(){
-		$(".email-2").fadeToggle();
-		$(".container-2").toggleClass("closed");
-		$(".container-2").toggleClass("open");
+	$container2.click(function() {
+		toggleAction($email2, $container2)
 	});
 });
-
-
